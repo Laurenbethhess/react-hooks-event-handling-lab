9f1be5d06d839ed3ceed01561758980dfd66050f
Lauren Hess <laurenbethhess@gmail.com>
2021-11-23 19:42:40 -0500

    commit tree b605c9604d65257d9b670cb39560403857b4d9a2

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,55 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+// function EyesOnMe() {
+
+//     function focus() {
+//         console.log('Good!')
+//     }
+
+//     function blur () {
+//         console.log('Hey! Eyes on me!')
+//     }
+
+//     let thing = true
+
+//     return (
+//         <div>
+//           <button
+//             onClick={() => {
+//                 if (thing) {
+//                     thing = false
+//                     blur()
+//                 }else {
+//                     thing = true
+//                     focus()
+//                 }
+//             }}>
+//             Eyes on me
+//           </button>
+//         </div>
+//       );
+// }
+
+function EyesOnMe() {
+
+    function focus() {
+        console.log('Good!')
+    }
+
+    function blur () {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <div>
+          <button onFocus={focus} onBlur={blur}>
+            Eyes on me
+          </button>
+        </div>
+      );
+}
+
+export default EyesOnMe
+
+
+
